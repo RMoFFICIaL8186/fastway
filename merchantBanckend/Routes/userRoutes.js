e2c221652b41789239f542c8c8b9397fd8024284
RMoFFICIal8186 <rmofficial8186@yahoo.com>
2025-07-27 10:57:31 +0530

    re created file

--- merchantBanckend/Routes/userRoutes.js
+++ merchantBanckend/Routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const route = express.Router();
+const { body, validationResult } = require('express-validator');
 
 route.get('/register', (req,res)=>{
     res.status(200).json({message: 'hello'});
@@ -10,8 +11,11 @@ route.post('/register',
     body('email').trim().isEmail().isLength({ min: 13 }),
     body('password').trim().isLength({ min: 4 }),
     async(req,res)=>{
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        res.status(200).json({message: 'User registered successfully!'});
 })
 
 module.exports = route;
-
-
